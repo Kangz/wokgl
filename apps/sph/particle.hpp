762509d019a96dc826fbfd58d98d3c5711b5bc43
Corentin Wallez <corentin@wallez.net>
2013-03-26 12:56:00 +0100

    Add a particles class to hold all the state of the particles

--- apps/sph/particle.hpp
+++ apps/sph/particle.hpp
@@ -2,6 +2,11 @@
 #define SPH_PARTICLE_H
 
 #include <string>
+#include "compute/Buffer.hpp"
+#include "compute/Context.hpp"
+#include "compute/gl.hpp"
+#include "renderer/Buffer.hpp"
+#include "renderer/Texture.hpp"
 
 #define PARTICLE_SIZE 10
 #define PARTICLE_FULLSIZE (PARTICLE_SIZE * sizeof(float))
@@ -18,6 +23,19 @@
 #define PARTICLE_ACCEL_Y 7
 #define PARTICLE_FORCE_X 8
 #define PARTICLE_FORCE_Y 9
+
+struct Particles{
+    compute::Buffer bufferCL;
+    renderer::TextureBuffer bufferGL;
+    renderer::Texture tex;
+
+    Particles(compute::Context context, float* data, int nParticles)
+    :bufferGL(renderer::BufferUsage::StaticDraw, nParticles * PARTICLE_FULLSIZE, data),
+    tex(renderer::TextureFormat::R32F){
+        this->tex.dataFromBuffer(this->bufferGL);
+        this->bufferCL = compute::bufferFromGL(context, this->bufferGL);
+    }
+};
 
 std::string getParticleHeader();
 
